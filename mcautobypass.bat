f92ea73a7cde89da3560a64f4d362aafb2faf18e
rhuda21 <165597973+rhuda21@users.noreply.github.com>
2024-05-25 11:06:01 +0100

    Add files via upload

---
+++ mcautobypass.bat
@@ -0,0 +1,15 @@
+cd "C:\Program Files"
+mkdir MCBypass
+cd MCBypass
+curl -O https://raw.githubusercontent.com/rhuda21/mcbypass/main/Windows.ApplicationModel.Store.dll
+mkdir backup
+cd backup
+copy C:\Windows\system32\Windows.ApplicationModel.Store.dll
+taskkill /F /IM  Gamebar.exe
+taskkill /F /IM  RuntimeBroker.exe
+taskkill /f /im Minecraft.Windows.exe
+taskkill /f /im WinStore.App.exe
+TAKEOWN /F C:\Windows\system32\Windows.ApplicationModel.Store.dll
+icacls C:\Windows\system32\Windows.ApplicationModel.Store.dll /grant %username%:F
+del C:\Windows\system32\Windows.ApplicationModel.Store.dll
+copy "C:\Program Files\MCBypass\Windows.ApplicationModel.Store.dll C:\windows\system32
