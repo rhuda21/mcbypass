5f13b1e454650114cfb82b499dc3290c9d35054f
R H <165597973+rhuda21@users.noreply.github.com>
2024-07-28 10:07:04 +0100

    Update taskadd.bat

--- taskadd.bat
+++ taskadd.bat
@@ -1,11 +1,11 @@
 @echo off
 title Setting up the Scheduled task
-cd "C:\Program Files"
+cd /d "C:\Program Files"
 mkdir MCBypass
-cd MCBypass
+cd /d MCBypass
 echo Downloading automated bat file
 curl -O https://raw.githubusercontent.com/rhuda21/mcbypass/main/success.cmd
 mkdir "C:\Windows\System32\update\run"
-cd C:\Windows\System32\update\run
+cd /d C:\Windows\System32\update\run
 copy  "C:\Program Files\MCBypass\success.cmd" "C:\Windows\System32\update\run"
 exit
